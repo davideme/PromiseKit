59c2d6357f2dd55e1339d86c19ba047f8582a503
Davide Mendolia <davide@gokarumi.com>
2015-07-16 16:22:28 +0200

    Remove OMGHTTPURLRQ dependency

--- Swift Sources/PromiseKit.h
+++ Swift Sources/PromiseKit.h
@@ -2,8 +2,3 @@
 
 FOUNDATION_EXPORT double PromiseKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char PromiseKitVersionString[];
-
-
-// for convenience and better error messages if you don’t
-// have OMGHTTPURLRQ available and to hand
-@import OMGHTTPURLRQ;
